4fe2b28ecad1f8f45d8d958bb1c6b3db3c024f21
Conor Gaffney <conorsg@gmail.com>
2015-07-16 17:03:47 -0500

    a minamal theme, not too different from the standard opa theme

--- plotters.R
+++ plotters.R
@@ -78,6 +78,57 @@ theme_opa <- function (base_size = 14, base_family = "")
     complete = TRUE)
 }
 
+theme_opa_minimal <- function (base_size = 14, base_family = "")
+{
+  theme(
+    line = element_line(colour = "black", size = 0.5, linetype = 1, lineend = "butt"),
+    rect = element_rect(fill = "white", colour = "black", size = 0.5, linetype = 1),
+    text = element_text(family = base_family, face = "plain", colour = "black", size = base_size, hjust = 0.5, vjust = 0.5, angle = 0, lineheight = 0.9),
+
+    axis.text = element_text(size = rel(0.6), colour = "grey70"),
+    strip.text = element_text(size = rel(0.8)),
+    axis.line = element_line(colour = "gray70"),
+    axis.text.x = element_text(vjust = 1, angle = 45, hjust = .97),
+    axis.text.y = element_text(hjust = 1),
+    axis.ticks = element_line(colour = "gray70"),
+    axis.title.x = element_text(),
+    axis.title.y = element_text(angle = 90),
+    axis.ticks.length = unit(0.15, "cm"),
+    axis.ticks.margin = unit(0.1, "cm"),
+
+    legend.background = element_rect(colour = NA),
+    legend.margin = unit(0.2, "cm"),
+    legend.key = element_rect(colour = NA, fill = "white"),
+    legend.key.size = unit(.8, "lines"),
+    legend.key.height = NULL,
+    legend.key.width = NULL,
+    legend.text = element_text(size = rel(0.75)),
+    legend.text.align = NULL,
+    legend.title = element_blank(),
+    legend.title.align = NULL,
+    legend.position = c(0,1),
+    legend.direction = "horizontal",
+    legend.justification = c(-0.02, 0.8),
+    legend.box = NULL,
+
+    panel.background = element_rect(fill = "white", colour = NA),
+    panel.border = element_blank(),
+    panel.grid.major = element_line(colour = "gray90"),
+    panel.grid.minor = element_line(colour = "gray90"),
+    panel.margin = unit(0.25, "lines"),
+    panel.margin.x = NULL,
+    panel.margin.y = NULL,
+
+    strip.background = element_rect(fill = "grey80", colour = NA),
+    strip.text.x = element_text(),
+    strip.text.y = element_text(angle = -90),
+
+    plot.background = element_rect(colour = "white"),
+    plot.title = element_text(size = rel(1.2), hjust = 0.05),
+    plot.margin = unit(c(1, 1, 1, 1), "lines"),
+    complete = TRUE)
+}
+
 #aesthetic variables
 lightBlue <- "#72d0ff"
 darkBlue <- "#005983"
